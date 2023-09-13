294c0de257eea5732788c08dc5f452cffbedbeae
akinori <aki10.working@gmail.com>
2023-09-13 13:09:10 +0900

    first commit

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -1,6 +1,6 @@
-import { SVGAttributes } from 'react';
+import { ImgHTMLAttributes } from 'react';
 
-export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
+export default function ApplicationLogo(props: ImgHTMLAttributes<HTMLImageElement>) {
     return (
       <img src="/storage/logo.jpg" alt="Logo" width={100} height={100} {...props} />
     );
